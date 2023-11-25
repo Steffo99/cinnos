d80caa306f97d146fc1aa171a0de5eab1d151a58
Stefano Pigozzi <me@steffo.eu>
2023-11-25 13:49:33 +0100

    Move

--- Player.gd
+++ Player.gd
@@ -2,31 +2,45 @@ extends CharacterBody3D
 class_name Player
 
 
-const SPEED = 5.0
-const JUMP_VELOCITY = 4.5
+@export var input_accel: float = 1.0
+@onready var current_input_accel = input_accel
 
-# Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
+@onready var gravity_accel: Vector3 = ProjectSettings.get_setting("physics/3d/default_gravity") * ProjectSettings.get_setting("physics/3d/default_gravity_vector")
+@export var jump_impulse: Vector3 = Vector3.UP * 100.0;
+
+
+@export var max_jumps = 1
+@onready var current_jumps = max_jumps
+
+
+func refill_jumps():
+	current_jumps = max_jumps
+
+
+func do_jump():
+	current_jumps -= 1
+	velocity += jump_impulse
+
 
 
 func _physics_process(delta):
-	# Add the gravity.
-	if not is_on_floor():
-		velocity.y -= gravity * delta
+	# Refilla salto
+	if is_on_floor():
+		refill_jumps()
+	
+	# Rileva salto
+	if Input.is_action_just_pressed("jump") and current_jumps > 0:
+		do_jump()
+	
+	# Applica gravità
+	velocity += gravity_accel
 
-	# Handle Jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+	# Applica input
+	var input_dir = Input.get_vector("move_left", "move_right", "move_up", "move_down")
+	var input_change = input_dir * input_accel
+	velocity.x += input_change.x
+	velocity.z += input_change.y
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
+	print(velocity)
 
 	move_and_slide()
